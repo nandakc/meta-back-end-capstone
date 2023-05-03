35b92de35f940e19413ced6a52f1c84f1e591a24
Nanda Chintalapati <nanda.chintalapati@siemens.com>
2023-05-03 13:53:26 -0700

    Added models and set-up MySQL.

--- LittleLemon/Restaurant/models.py
+++ LittleLemon/Restaurant/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class menu(models.Model):
+    Title = models.CharField(max_length=255)
+    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    Inventory = models.IntegerField()
+
+class booking(models.Model):
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField()
+    BookingData = models.DateTimeField
